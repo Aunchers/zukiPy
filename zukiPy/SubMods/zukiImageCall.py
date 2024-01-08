0858c82ccb40a43565deca2f01453ba010c6aba7
1_aunchers <91509158+Launchers-1@users.noreply.github.com>
2024-01-08 16:58:43 -0500

    ImageCallFix
    Fixed Image Call in SubModules

--- zukiPy/SubMods/zukiImageCall.py
+++ zukiPy/SubMods/zukiImageCall.py
@@ -2,23 +2,32 @@ import requests
 class zukiImageCall:
   def __init__(self, api_key):
     self.api_key = api_key
-  def image_data(self, prompt, generations, size):
+  def image_data(self, prompt, generations, model, negative_prompt, width, height):
     data = {
       "prompt": prompt,
       "n": generations,
-      "size": size
+      "size": str(width) + "x" + str(height),
+      "model": model,
+      "negative_prompt": negative_prompt,
+      "width": width,
+      "height": height
     }
     return data
 
-  async def image_call(self, prompt, generations, size, endpoint):
-    try:
-      response = requests.post(endpoint, headers={
-         'Content-Type': 'application/json',
-         'Authorization': f'Bearer {self.api_key}'
-      },
-      data=self.image_data(prompt, generations, size))
-      imgUrl = response.json()
-      return imgUrl['data'][0]['url']
-      
-    except Exception as e:
-      print(f"An error occurred: {e}")
+  async def image_call(self, prompt, generations, endpoint, model, negative_prompt, width, height):
+   try:
+       response = requests.post(endpoint, headers={
+           'Content-Type': 'application/json',
+           'Authorization': f'Bearer {self.api_key}'
+       },
+       json=self.image_data(prompt, generations, model, negative_prompt, width, height))
+       imgUrl = response.json()
+       if 'detail' in imgUrl:
+           raise ValueError(imgUrl)
+       else:
+           return imgUrl['data'][0]['url']
+
+   except Exception as e:
+       print(f"An error occurred: {e}")
+       raise
+
