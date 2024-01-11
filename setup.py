3c35a6663d10bc97d696765c4cab323b64ecefef
1_aunchers <91509158+Launchers-1@users.noreply.github.com>
2024-01-11 18:43:07 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='zukiPy',
+    version='1.0.0',
+    description='A Python wrapper for zukijourney',
+    author='1_aunchers',
+    author_email='1_aunchers@proton.me',
+    packages=find_packages(),
+    url='https://github.com/Launchers-1/zukiPy',
+)
