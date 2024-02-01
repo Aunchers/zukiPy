8f9668ee20047ba981adc4a4e67eb83affd1515d
Sabsterrexx <51344922+Sabsterrexx@users.noreply.github.com>
2024-01-31 20:45:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='zukiPy',
     version='1.0.0',
     description='A Python wrapper for zukijourney',
-    author='1_aunchers',
+    author='1_aunchers, Sabsterrexx', 
     author_email='1_aunchers@proton.me',
     packages=['zukiPy', 'zukiPy.MainMods', 'zukiPy.SubMods'],
     url='https://github.com/Launchers-1/zukiPy',
