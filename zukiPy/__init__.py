228a137f097bd7945750870ab4ab2557f937a65e
1_aunchers <91509158+Launchers-1@users.noreply.github.com>
2024-02-04 13:54:55 -0500

    Update __init__.py

--- zukiPy/__init__.py
+++ zukiPy/__init__.py
@@ -3,7 +3,7 @@ from zukiPy.MainMods.zukiImage import zukiImage
 
 
 class zukiCall:
-    def __init__(self, api_key, api_key_backup=None, model="gpt-3.5-turbo",  systemPrompt="You are a helpful assistant.", temperature=0.7):
+    def __init__(self, api_key, model="gpt-3.5-turbo",  systemPrompt="You are a helpful assistant.", temperature=0.7, api_key_backup=None):
         self.api_key = api_key
         self.api_key_backup = api_key_backup
         self.model = model
