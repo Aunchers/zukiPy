ad56b1a2ed0c1e5f6ddb47bb255f4b40715fe750
Sabsterrexx <51344922+Sabsterrexx@users.noreply.github.com>
2025-01-18 21:11:09 -0500

    Fix default parameter for image calls, sdxl no longer supported.

--- zukiPy/MainMods/zukiImage.py
+++ zukiPy/MainMods/zukiImage.py
@@ -14,14 +14,14 @@ class zukiImage:
     def change_backup_endpoint(self, endpoint):
         self.api_endpoint_backup = endpoint
 
-    async def generateImage(self, prompt, generations=1, size=250, model = "sdxl"):
+    async def generateImage(self, prompt, generations=1, size=250, model = "flux-schnell"):
         imgUrl = await self.api_caller.image_call(prompt, generations, self.api_endpoint, model, negative_prompt="", width=1024, height=1024)
         if "details" in str(imgUrl):
             return ValueError(imgUrl)
         else:
             return imgUrl
 
-    async def generateImageBackup(self, prompt, generations=1, size=250, model="sdxl"):
+    async def generateImageBackup(self, prompt, generations=1, size=250, model="flux-schnell"):
         imgUrl = await self.api_caller_backup.image_call(prompt, generations, self.api_endpoint_backup, model, negative_prompt="", width=1024, height=1024)
         if "details" in str(imgUrl):
             return ValueError(imgUrl)
